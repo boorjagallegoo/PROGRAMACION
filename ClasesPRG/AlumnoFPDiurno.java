27810db871e9bac10b292a0f7799c14bf962d371
Borja G <116759401+boorjagallegoo@users.noreply.github.com>
2022-12-02 17:47:02 +0100

    Delete ClasesPRG directory

--- ClasesPRG/AlumnoFPDiurno.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public class AlumnoFPDiurno extends ArrayList<AlumnoFP> {
-    
-}
-// Esto no entra en el examen.
--- ClasesPRG/Ejercicios Repaso/Ejercicios T2.2/StringUtils.java
+++
@@ -1,8 +0,0 @@
-
-public interface StringUtils {
-
-    static String capitalize(String strIn) {
-        return null;
-    }
-
-}
